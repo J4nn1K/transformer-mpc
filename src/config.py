d649eb708b0ceb6619a2bc798c9c90e91734221e
J4nn1K <jannikjorge@grothusen.de>
2023-04-29 00:02:48 -0700

    Update config format

--- src/config.py
+++ src/config.py
@@ -2,11 +2,25 @@ config = {
   'data': {
     'path': '../data/robot_field_data.pt',
     'dt': 0.025,
+    'shape': (100, 100, 1) # HxWxC
   },
-  'control': {
-    'horizon': 10,
-    'dt': 0.1
-  },
+  'model': {
+    'patches': (4,4),
+    'transformer': {
+      'num_layers': 3,
+      'mlp_dim': 64,
+      'num_heads': 1,
+      'dropout_rate': 0.1,
+      'attention_dropout_rate': 0.1,
+    },
+    'solver': {
+      'dt': 0.1,
+      'horizon': 10,
+    },
+    'hidden_size': 42,
+    'num_output': 42,
+    'representation_size': 64,
+  }, 
   'training': {
     'train_ratio': 0.8,
     'batch_size': 1
