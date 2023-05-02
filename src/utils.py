f2fd05092d27f8184a31b27209806b001e324590
J4nn1K <jannik@grothusen.de>
2023-05-02 15:47:41 -0700

    Add function to generate video from image data

--- src/utils.py
+++ src/utils.py
@@ -41,6 +41,34 @@ def save_grids_as_video(grids, indices, video_filename, freq_out, upscale_factor
     video_writer.release()
     print("Video saved as", video_filename)
     
+def save_images_as_video(images, indices, video_filename, freq_out, upscale_factor=1):
+    if not indices:
+        print("No indices provided. Nothing to save.")
+        return
+
+    # Get the dimensions of the grids and upscale them
+    height, width, _ = images[0].shape
+    upscaled_width = int(width * upscale_factor)
+    upscaled_height = int(height * upscale_factor)
+
+    # Create a video writer
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter(video_filename, fourcc, freq_out, (upscaled_width, upscaled_height), isColor=True)
+
+    for index in indices:
+        # Convert the grid to the correct format for the video writer
+        grid_BGR = cv2.cvtColor(images[index], cv2.COLOR_RGB2BGR)
+
+        # Resize the frame
+        grid_resized = cv2.resize(grid_BGR, (upscaled_width, upscaled_height), interpolation=cv2.INTER_AREA)
+
+        # Write the frame to the video
+        video_writer.write(grid_resized)
+
+    # Close the video writer
+    video_writer.release()
+    print("Video saved as", video_filename)
+    
 
 def get_model_output(model, state, dataset, indices):
   commands ={'target': [], 'pred': []}
